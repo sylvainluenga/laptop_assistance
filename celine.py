29c482780f44301ad38ec9981d9d4f0201a695f0
Alex Sylvain Luenga <38809975+0788611515@users.noreply.github.com>
2022-03-10 20:37:09 +0200

    still under building

---
+++ celine.py
@@ -0,0 +1,87 @@
+import pyttsx3  # pip install pyttsx3
+import http.client
+import numpy as np
+import datetime
+import speech_recognition as sr  # pip install SpeechRecognition
+import wikipedia  # pip install wikipedia
+import autopep8  # -m pip install -U autopep8
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'voices[0].id')
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak("the current time is")
+    speak(Time)
+
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+    speak("the current date is")
+    speak(date)
+    speak(month)
+    speak(year)
+
+
+def wishme():
+    speak("Welcome back Alex")
+    time()
+    date()
+    hour = datetime.datetime.now().hour
+    if hour >= 6 and hour < 12:
+        speak("Good morning Alex")
+    elif hour >= 12 and hour < 18:
+        speak("good afternoon Alex")
+    elif hour >= 18 and hour < 24:
+        speak("Good Evening Alex")
+    else:
+        speak("Good night Alex")
+
+    speak("Celine at your service, please tell me How can I assist you")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recongnizning...")
+        query = r.recognize_google(audio, language='en-ZA')
+        print(query)
+
+    except Exception as e:
+        print(e)
+        speak("Please repeat that again, Alex...")
+
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishme()
+    while True:
+        query = takeCommand().lower()
+        if 'time' in query:
+            time()
+        elif 'date' in query:
+            date()
+        elif 'wikipidia' in query:
+            speak("Searching...")
+            query = query.replace("wikipedia", "")
+            result = wikipedia.summary(query, sentences=2)
+            print(result)
+            speak(result)
+        elif 'offline' in query:
+            quit()
