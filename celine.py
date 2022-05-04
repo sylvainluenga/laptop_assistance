86a2636195c72c1e60de660891c1f4eada196235
Alex Sylvain Luenga <38809975+0788611515@users.noreply.github.com>
2022-05-04 10:35:18 +0200

    sendEmail

--- celine.py
+++ celine.py
@@ -72,8 +72,9 @@ def takeCommand():
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
-    server.starttls
-    
+    server.starttls()
+    server.
+
 
 
 
